fd826fab43e8932c8af25f763d8a3ee494cc5ac5
Max Aubain <max.aubain@gmail.com>
2019-07-17 19:30:50 +0200

    Migrate database after creation of kindergarten migration, and create spec file for kindergarten model.

--- app/models/kindergarten.rb
+++ app/models/kindergarten.rb
@@ -1,2 +1,5 @@
 class Kindergarten < ApplicationRecord
+    validates :name, presence: true
+    validates :kommun, presence: true
+    validates :address, presence: true
 end
--- spec/models/kindergarten_spec.rb
+++ spec/models/kindergarten_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Kindergarten, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'DB table' do
+    it { is_expected.to have_db_column :name }
+    it { is_expected.to have_db_column :kommun }
+    it { is_expected.to have_db_column :address }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :kommun }
+    it { is_expected.to validate_presence_of :address }    
+  end
+
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(FactoryBot.create(:kindergarten)).to be_valid 
+    end
+  end
 end
