92fa65c1165b2ea4dabc9ad234ba7728c9030146
Max Aubain <max.aubain@gmail.com>
2019-07-18 15:20:32 +0200

    Transplant params and create methods from playdates controller to kindergartens controller, rename partial 'kindergartens list' view according to conventions for partial views.

--- app/controllers/kindergartens_controller.rb
+++ app/controllers/kindergartens_controller.rb
@@ -1,6 +1,15 @@
 class KindergartensController < ApplicationController
+  def index 
+    @kindergartens = Kindergarten.all
+  end  
 
-    def index 
-      @kindergartens = Kindergarten.all
-    end  
+  def create
+    @kindergarten = Kindergarten.new(kindergarten_params)
+  end
+
+  private
+
+  def kindergarten_params
+    params.require(:kindergarten).permit(:name, :kommun, :address)
+  end
 end
--- app/controllers/playdates_controller.rb
+++ app/controllers/playdates_controller.rb
@@ -1,16 +1,2 @@
 class PlaydatesController < ApplicationController
-  def index
-    @kindergartens = Kindergarten.all
-  end
-
-  def create
-    @kindergarten = Kindergarten.new(kindergarten_params)
-  end
-
-  private
-
-  def kindergarten_params
-    params.require(:kindergarten).permit(:name, :kommun, :address)
-  end
-
 end
