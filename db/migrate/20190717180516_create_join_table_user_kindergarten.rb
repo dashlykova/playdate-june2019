1ff2e748fb0f253af2fade9995396c791d00c695
Max Aubain <max.aubain@gmail.com>
2019-07-19 11:35:51 +0200

    Delete Join Table for users and kindergartens migration.

--- db/migrate/20190717180516_create_join_table_user_kindergarten.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableUserKindergarten < ActiveRecord::Migration[5.2]
-  def change
-    create_join_table :users, :kindergartens do |t|
-      # t.index [:user_id, :kindergarten_id]
-      # t.index [:kindergarten_id, :user_id]
-    end
-  end
-end
